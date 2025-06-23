2c6e6d513ac0d6ff946660fbcb0d17ff1ebf3c61
Atulcynoteck <atul.tiwari@cynoteck.com>
2025-06-23 15:11:25 +0530

    Update .gitignore to exclude Pods and user data

--- Pods/Target Support Files/Pods-GithubActionDemo-GithubActionDemoUITests/Pods-GithubActionDemo-GithubActionDemoUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_GithubActionDemo_GithubActionDemoUITests : NSObject
-@end
-@implementation PodsDummy_Pods_GithubActionDemo_GithubActionDemoUITests
-@end
--- Pods/Target Support Files/Pods-GithubActionDemoTests/Pods-GithubActionDemoTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_GithubActionDemoTests : NSObject
-@end
-@implementation PodsDummy_Pods_GithubActionDemoTests
-@end
