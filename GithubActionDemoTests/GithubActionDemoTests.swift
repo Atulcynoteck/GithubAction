2406cecf70be4a8700366696979aa55b7728e323
Atulcynoteck <atul.tiwari@cynoteck.com>
2025-06-23 14:40:43 +0530

    Clean init of valid Xcode project

---
+++ GithubActionDemoTests/GithubActionDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  GithubActionDemoTests.swift
+//  GithubActionDemoTests
+//
+//  Created by user on 23/06/25.
+//
+
+import Testing
+@testable import GithubActionDemo
+
+struct GithubActionDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
